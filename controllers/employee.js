6953fcfa6db443e11cc3ef51dc38ec71a45bc44e
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-06-15 19:19:28 +0300

    Add attributes to Employee, Signin and middlewares

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,10 +2,22 @@ const jwt = require('jsonwebtoken');
 const Employee = require('../DB/models/employee');
 const { AppError } = require('../lib/index');
 
-
+const generateToken = (employee) => {
+  const token = jwt.sign({ userName: employee.userName, userId:employee._id, role:employee.role} , process.env.TOKEN_KEY, { expiresIn: '7d' } )
+  return token;
+}
 const create = (data) =>  Employee.create(data);
 
+const signIn = async (employee) => {
+  const user = await Employee.findOne({ userName : employee.userName})
+  if(!user)  throw new AppError('Invalid username', 400);
+  const valid = user.verifyPassword(employee.password);
+  if (!valid) throw new AppError('Invalid password', 400);
+  return {token: generateToken(user), user}
+  
+}
 
 module.exports = {
   create,
+  signIn
 };
--- routes/admin_employee.js
+++ routes/admin_employee.js
@@ -3,28 +3,41 @@ const { asycnWrapper }  = require('../lib/index');
 
 const { employeeController } = require('../controllers/index');
 const { usersValidator } = require('../Validations');
+const {adminAuth} = require('../middlewares/auth');
 // const { validate } = require('../middlewares/validation');
+
 
 
 const router = express.Router();
 
-router.post('/register', async (req, res, next) => {
+// Add Employee
+router.post('/', adminAuth, async (req, res, next) => {
     const { body: {
       firstName, lastName, userName, email, password, nationalId,
-      role,hireDate,position,dep_id,salary,phoneNumber,
+      role,hireDate,position,depId,salary,phoneNumber,jobType,gender,
       address:{street,city},
-      pImage, 
+      academicQualifications:{degree,institution,year},pImage, 
     }} = req;
     const user = employeeController.create({
       firstName, lastName, userName, email, password, nationalId,
-      role,hireDate,position,dep_id,salary,phoneNumber,
-      address:{street,city},
-      pImage, 
+      role,hireDate,position,depId,salary,phoneNumber,jobType,gender,
+      address:{street,city},academicQualifications:{degree,institution,year},pImage, 
     });
     const [err, data] = await asycnWrapper(user);
     if (err) return next(err);
     res.status(201).json({ status:'success', data });
   });
+
+// Sign in 
+router.post('/signin', async (req, res) => {
+  try {
+    const { userName, password } = req.body;    
+    const data = await employeeController.signIn({ userName, password });    
+    res.status(200).json({status:'success', data })
+  } catch (err) {
+    next(err);
+  }
+});
 
 
 module.exports = router;
