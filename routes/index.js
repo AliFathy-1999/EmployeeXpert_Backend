f8d2ed7694f196069e914a2fb7b4ef22e9590e6b
NADAHISHAM22 <nada.hisham.elsaid@gmail.com>
2023-06-27 02:18:12 +0300

    communications

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const employeeRoutes = require("./emloyee");
-const depRoutes = require("./department");
-const commRoute = require("./communications")
+const employeeRoutes = require('./emloyee');
+const depRoutes = require('./department');
+const commRoute = require('./communications')
 const adminEmpRoutes = require('./admin_employee');
 const adminDepRoutes = require('./admin_department');
 const employeeVacationsRoutes = require('./vacation');
 const salaryRoutes = require ('./payroll');
 const EmployeeSalaryRoutes = require ('./userSalary');
 
-router.use("/dep", depRoutes);
-router.use("/communications",commRoute)
+router.use('/dep', depRoutes);
+router.use('/communications', commRoute)
 router.use('/', employeeRoutes);
 router.use('/admin/emp/', adminEmpRoutes);
 router.use('/admin/dep/', adminDepRoutes);
@@ -18,9 +18,6 @@ router.use('/dep/', depRoutes);
 router.use('/vacations', employeeVacationsRoutes);
 router.use('/salary', salaryRoutes);
 router.use('/employee/salary', EmployeeSalaryRoutes);
-
-// router.use("/admin", adminRoute);
-
 
 module.exports = router;
 
