6d450952e7087f1f99829a9a9ede55ff719212a9
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-06-27 17:16:57 +0300

    refactor update employee

--- routes/admin_employee.js
+++ routes/admin_employee.js
@@ -38,22 +38,22 @@ router.post('/', validate(employeesValidator.signUp), async (req, res, next) => 
 
   // Admin update employee data
 
-  router.patch('/:id', validate(employeesValidator.signUp), validate(employeesValidator.checkvalidID), async (req, res, next) => {
+  router.put('/:id', validate(employeesValidator.signUp), validate(employeesValidator.checkvalidID), async (req, res, next) => {
     const { params : { id }} = req;
     const {  
       firstName, lastName, nationalId,
       role, hireDate, position, depId, salary, phoneNumber, jobType, gender,
-      address, academicQualifications, pImage, 
+      address, academicQualifications : {college, degree, institution, year}, pImage, 
     } = req.body;
-    
-    const department = await Department.findOne({_id : depId});
-    if (!department) 
-      return next(new AppError (`No Department with ID ${depId}`, 400));
-      
+    if(depId){
+      const department = await Department.findOne({_id : depId});
+      if (!department) 
+        return next(new AppError (`No Department with ID ${depId}`, 400));
+    }
     const user = employeeController.updateEmployee(id, {
       firstName, lastName, nationalId,
       role, hireDate, position, depId, salary, phoneNumber, jobType, gender,
-      address, academicQualifications, pImage
+      address, academicQualifications : {college, degree, institution, year}, pImage
     });
     const [err, data] = await asycnWrapper(user);
     if (err) return next(err);
@@ -61,9 +61,21 @@ router.post('/', validate(employeesValidator.signUp), async (req, res, next) => 
     res.status(201).json({ status : 'success', data });
   });
 
+
+  router.patch('/acad/:id', validate(employeesValidator.checkvalidID), async (req, res, next) => {
+    const { params : { id }} = req;
+    const { college, degree, institution, year} = req.body;
+    console.log(req.body);
+    const user = employeeController.updateAcademicQual(id, {college, degree, institution, year});
+    const [err, data] = await asycnWrapper(user);
+    if (err) return next(err);
+    if (!data) return next(new AppError (`No Employee with ID ${id}`, 400));
+    res.status(201).json({ status : 'success', data });
+});
+
 // Admin delete employee (USER)
 
-  router.delete('/:id', async (req, res, next) => {
+  router.delete('/:id', validate(employeesValidator.checkvalidID), async (req, res, next) => {
     const { params : { id }} = req;
     const user = employeeController.deleteEmployee(id);
     await Payroll.deleteOne({ employeeId : id });
@@ -85,7 +97,7 @@ router.get('/', async (req, res, next) => {
 
 // Get specified employee (USER or Admin)
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', validate(employeesValidator.checkvalidID), async (req, res, next) => {
   const { id } = req.params;
   const user = employeeController.employeeDetails(id);
   const [err, data] = await asycnWrapper(user);
