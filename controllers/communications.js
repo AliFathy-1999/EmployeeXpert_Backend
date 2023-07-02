c5852d54e5f69160beaea6a49e52abc42fb56d29
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-07-02 23:50:09 +0300

    refactor communication

--- controllers/communications.js
+++ controllers/communications.js
@@ -1,46 +1,30 @@
 const Communications = require('../DB/models/comunications');
 
+const create = (data) => {
+    return Communications.create(data)
+};
 
-const create = (data) => Communications.create(data);
-const findMyMessages = async ( userId) => {
-    try{
-
-        const messages = await Communications.find({ employee : userId})
+const findMyMessage = async ( userId) => {
+        const messages = Communications.find({ employee : userId})
         return messages  
     }
-    catch(erorr){
-        return "no messages"
-    }
-
-}
-const findEmpMessages = async (data , userId) => {
-    try{
-        console.log(data,userId)
-     const messages = await Communications.find({
-        employee : data , sender : userId
-        
-      });
-   
+const findEmpMessages = async (data, userId) => {
+    const messages = Communications.find({ employee : data, sender : userId})
     return messages   
-    }
-    catch(erorr){
-        return "no messages"
-    }
-    
 }
 
 const findDepMessages = async (data) => {
-    const messages = await Communications.find({ department : data });
+    const messages = Communications.find({ department : data });
     return messages
 }
 
 const findLastAouncement = async () => {
-    const messages = await Communications.find({ isForAll : true}).sort({ createdAt : -1}).limit(1);
+    const messages = Communications.find({ isForAll : true}).sort({ createdAt : -1}).limit(1);
     return messages
 }
 
 const findAllAnouncements = async () => {
-    const messages = await Communications.find({ isForAll : true})
+    const messages = Communications.find({ isForAll : true})
     return messages
 }
 
@@ -48,7 +32,7 @@ module.exports = {
   create,
   findDepMessages,
   findEmpMessages,
-  findLastAouncement ,
-  findAllAnouncements ,
-  findMyMessages
+  findLastAouncement,
+  findAllAnouncements,
+  findMyMessage
 };
