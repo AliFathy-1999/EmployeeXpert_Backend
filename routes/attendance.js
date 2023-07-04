01a3f14e13adfeb389daabe8ae35c62d928ed6b6
my name <hendmohammed10420@gmail.com>
2023-07-04 23:11:36 +0300

    last commit in attendance

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,23 +1,36 @@
 const express = require('express');
-const { attendanceController }  = require('../controllers/index');
+const { attendanceController } = require('../controllers/index');
 
 const { adminAuth, Auth } = require('../middlewares/auth');
 
 const router = express.Router();
+
 // Check-in route
-router.post('/checkin/',attendanceController.checkIn);
+
+router.post('/checkin/', attendanceController.checkIn);
+
 // Check-out route
-router.post('/checkout/',  attendanceController.checkOut);
+
+router.post('/checkout/', attendanceController.checkOut);
+
 // get all attendances for employee 
+
 router.get('/all/', attendanceController.getAllAttendancesOfEmployee);
 
 // Create a new attendance record
+
 router.post('/', adminAuth, attendanceController.create);
+
 // Get a specific attendance record by ID
+
 router.get('/:id', adminAuth, attendanceController.getAttendanceById);
+
 // Update a specific attendance record by ID
+
 router.put('/:id', adminAuth, attendanceController.updateAttendanceById);
+
 // Delete a specific attendance record by ID
+
 router.delete('/:id', adminAuth, attendanceController.deleteAttendanceById);
 
 
