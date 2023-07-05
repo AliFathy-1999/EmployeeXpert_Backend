19a5757e5967fabbde70574be0c9cac1e36c7c81
MennaRaafat <menna.raafat.hamed@commerce.helwan.edu.eg>
2023-07-05 05:27:16 +0300

    Refactor

--- controllers/vacation.js
+++ controllers/vacation.js
@@ -449,6 +449,7 @@ const removeVacation = async (req, res) => {
 
 const applyForVacationByAdmin = async (req, res) => {
   try {
+    console.log(req.body)
     const { employeeId } = req.body;
     const empVacation = await Vacation.find({ employeeId });
     let totalDaysSum;
